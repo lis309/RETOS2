4376ae1819fac26168589125008aa4b1d6ce0cef
lis309 <127986044+lis309@users.noreply.github.com>
2023-03-17 08:51:38 -0500

    Add files via upload

--- RETOS2/src/reto3.java
+++ RETOS2/src/reto3.java
@@ -24,11 +24,14 @@ public class reto3 {
         
         //mensajes con el resultado
         System.out.println("Catálogo de productos");
-        System.out.println(" 0,0  "+nombre[0][0]+"  $"+precio[0][0]+" | "+" 0,1  "+nombre[0][1]+"  $"+precio[0][1]+" | "+" 0,2  "+nombre[0][2]+"  $"+precio[0][2]+" | "+" 0,3  "+nombre[0][3]+"  $"+precio[0][3]);
-        System.out.println(" 1,0  "+nombre[1][0]+"  $"+precio[1][0]+" | "+" 1,1  "+nombre[1][1]+"  $"+precio[1][1]+" | "+" 1,2  "+nombre[0][2]+"  $"+precio[1][2]+" | "+" 1,3  "+nombre[1][3]+"  $"+precio[1][3]);
-        System.out.println(" 2,0  "+nombre[2][0]+"  $"+precio[2][0]+" | "+" 2,1  "+nombre[2][1]+"  $"+precio[2][1]+" | "+" 2,2  "+nombre[0][2]+"  $"+precio[2][2]+" | "+" 2,3  "+nombre[2][3]+"  $"+precio[2][3]);
-        System.out.println(" 3,0  "+nombre[3][0]+"  $"+precio[3][0]+" | "+" 3,1  "+nombre[3][1]+"  $"+precio[3][1]+" | "+" 3,2  "+nombre[0][2]+"  $"+precio[3][2]+" | "+" 3,3  "+nombre[3][3]+"  $"+precio[3][3]);
-
+        for (int fila=0;fila<4;fila++) {
+            System.out.print("| ");
+            for (int columna=0;columna<4;columna++) {
+                System.out.print(fila+""+columna+" "+nombre[fila][columna]+"  $"+precio[fila][columna]+" | ");
+            }
+            System.out.println(" ");
+        }
+        
         Capturar.close();
     }
 }
